a89853f741036a5f0b38d4f3e2891f5a9af64fb9
buddhdev-jay <jay.b@simformsolutions.com>
2022-05-09 17:51:56 +0530

    Sign In Screen Completed

---
+++ app/src/main/java/com/example/androiduidesign/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.androiduidesign.utils
+
+const val THIRTYONE = 31
+const val THIRTYEIGHT = 38
+const val TWENETYFOUR = 24
+const val ONE = 1
+const val ZERO = 0
+const val TWENTYSIX = 26
+const val NINETEEN = 19
+const val TWENTY = 20
+const val THIRTYTHREE = 33
+const val TWOTHOUSAND = 2000
