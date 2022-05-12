30cca0de760ede95adb526330f1e616f337bca68
buddhdev-jay <jay.b@simformsolutions.com>
2022-05-12 11:52:45 +0530

    PhoneNumber issue Resolved

--- app/src/main/java/com/example/androiduidesign/authencation/SignInActivity.kt
+++ app/src/main/java/com/example/androiduidesign/authencation/SignInActivity.kt
@@ -3,22 +3,71 @@ package com.example.androiduidesign.authencation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.androiduidesign.R
 import com.example.androiduidesign.databinding.ActivityOnBoardingBinding
 import com.example.androiduidesign.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
-
     lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        supportActionBar?.hide()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
-
-        binding.textviewSignup.setOnClickListener {
+        intialSetup()
+        binding.textviewNotHaveAccount.setOnClickListener {
             startActivity(Intent(this,SignupActivity::class.java))
         }
+        binding.apply {
+            btnSignIn.setOnClickListener {
+                if(this.ediTxtSignInEmail.toString().isEmpty()){
+                    Toast.makeText(this@SignInActivity,getString(R.string.toast_email_empty), Toast.LENGTH_SHORT).show()
+                } else if (this.editxtSignInPassword.toString().isEmpty()){
+                    Toast.makeText(this@SignInActivity,getString(R.string.toast_password_empty),Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@SignInActivity,getString(R.string.sign_in_btn_clicked),Toast.LENGTH_SHORT).show()
+                }
+            }
+            btnFaceId.setOnClickListener {
+                Toast.makeText(this@SignInActivity,getString(R.string.face_id_clicked),Toast.LENGTH_SHORT).show()
+            }
+            btnFacebook.setOnClickListener {
+                Toast.makeText(this@SignInActivity,getString(R.string.facebook_clicked),Toast.LENGTH_SHORT).show()
+            }
+            btnGoogle.setOnClickListener {
+                Toast.makeText(this@SignInActivity,getString(R.string.google_clicked),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setSpannableText() {
+        val spannable = SpannableString(binding.textviewNotHaveAccount.text)
+        val clickableSpan2: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(p0: View) {
+                val signInIntent = Intent(this@SignInActivity,SignupActivity::class.java)
+                startActivity(signInIntent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                ds.setColor(ContextCompat.getColor(applicationContext, R.color.green_500))
+                ds.bgColor = ContextCompat.getColor(applicationContext, R.color.white)
+            }
+        }
+        spannable.setSpan(clickableSpan2, 24, 31, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.textviewNotHaveAccount.text = spannable
+        binding.textviewNotHaveAccount.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun intialSetup() {
+        supportActionBar?.hide()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        setSpannableText()
     }
 }
--- app/src/main/java/com/example/androiduidesign/authencation/SignupActivity.kt
+++ app/src/main/java/com/example/androiduidesign/authencation/SignupActivity.kt
@@ -5,15 +5,24 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
 import android.text.TextWatcher
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.androiduidesign.R
 import com.example.androiduidesign.databinding.ActivitySignupBinding
 import com.example.androiduidesign.onboardingscreen.OnBoardingActivity
-import kotlinx.android.synthetic.main.activity_signup.imgview_backarrow
+import kotlinx.android.synthetic.main.activity_signup.btn_signup
+import kotlinx.android.synthetic.main.activity_signup.img_view_back_arrow
 
 
 class SignupActivity : AppCompatActivity() {
@@ -24,26 +33,49 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         intialSetup()
-        imgview_backarrow.setOnClickListener {
+        img_view_back_arrow.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
             finish()
         }
+        btn_signup.setOnClickListener {
+            binding.apply {
+                if(this.ediTxtSignupFullName.toString().isEmpty()) {
+                    Toast.makeText(this@SignupActivity,getString(R.string.toast_name_empty),Toast.LENGTH_SHORT).show()
+                } else if(this.ediTxtSignupEmail.toString().isEmpty()) {
+                    Toast.makeText(this@SignupActivity,getString(R.string.toast_email_empty),Toast.LENGTH_SHORT).show()
+                } else  if(this.ediTxtSignupPassword.toString().isEmpty()) {
+                    Toast.makeText(this@SignupActivity,getString(R.string.toast_password_empty),Toast.LENGTH_SHORT).show()
+                } else if (this.ediTxtSignupConfirmPassword.toString().isEmpty()) {
+                        Toast.makeText(this@SignupActivity,getString(R.string.toast_confirm_password),Toast.LENGTH_SHORT).show()
+                } else if (this.ediTxtSignupPhoneNumber.toString().isEmpty()){
+                    Toast.makeText(this@SignupActivity,getString(R.string.toast_phone_empty) ,Toast.LENGTH_SHORT).show()
+                } else if (this.ediTxtSignupPhoneNumber.length() < 10 ){
+                    Toast.makeText(this@SignupActivity,getString(R.string.toast_valid_phone),Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@SignupActivity,getString(R.string.signup_btn_clicked),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         signupViewModel.password.observe(this) { password ->
-            if (password.length >= 8) {
-                var passwordStausupdate = 1
-                if (password.toString().contains(getString(R.string.numeric_regex).toRegex())) {
-                    passwordStausupdate += 1
+            password.apply {
+                if (this.isNotEmpty()) {
+                    var passwordstatusupdate = 1
+                    if (this.length >= 8) {
+                        if (this.toString().contains(getString(R.string.numeric_regex).toRegex())) {
+                            passwordstatusupdate += 1
+                        }
+                        if (this.toString().contains(getString(R.string.small_alphabet_regex).toRegex())) {
+                            passwordstatusupdate += 1
+                        }
+                        if (this.toString().contains(getString(R.string.upper_alphabet_regex).toRegex())) {
+                            passwordstatusupdate += 1
+                        }
+                    }
+                    signupViewModel.passwordStatus.value = passwordstatusupdate
+                } else {
+                    signupViewModel.passwordStatus.value = 0
                 }
-                if (password.toString().contains(getString(R.string.small_alphabet_regex).toRegex())) {
-                    passwordStausupdate += 1
-                }
-                if (password.toString().contains(getString(R.string.upper_alphabet_regex).toRegex())) {
-                    passwordStausupdate += 1
-                }
-                signupViewModel.passwordStatus.value = passwordStausupdate
-            } else {
-                signupViewModel.passwordStatus.value = 0
             }
         }
     }
@@ -51,12 +83,31 @@ class SignupActivity : AppCompatActivity() {
     private fun intialSetup() {
         supportActionBar?.hide()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
-        adapter = ArrayAdapter.createFromResource(this, R.array.countryCodes, android.R.layout.simple_spinner_item)
+        adapter = ArrayAdapter.createFromResource(this, R.array.countryCodes, android.R.layout.simple_spinner_dropdown_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.apply {
             viewModel = signupViewModel
             spinnerCountryCode.adapter = adapter
             lifecycleOwner = this@SignupActivity
         }
+        setSpannableText()
+    }
+
+    private fun setSpannableText() {
+        val spannable = SpannableString(binding.textviewNotHaveAccount.text)
+        val clickableSpan2: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(p0: View) {
+                val signInIntent = Intent(this@SignupActivity,SignInActivity::class.java)
+                startActivity(signInIntent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                ds.setColor(ContextCompat.getColor(applicationContext, R.color.green_500))
+                ds.bgColor = ContextCompat.getColor(applicationContext, R.color.white)
+            }
+        }
+        spannable.setSpan(clickableSpan2, 19, 26, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.textviewNotHaveAccount.text = spannable
+        binding.textviewNotHaveAccount.movementMethod = LinkMovementMethod.getInstance()
     }
 }
