ac27ab7ffe716d10af42736a596fa65d8d70d6d8
buddhdev-jay <jay.b@simformsolutions.com>
2022-05-13 16:10:10 +0530

    New Password Screen Done

--- app/src/main/java/com/example/androiduidesign/SplashScreenActivity.kt
+++ app/src/main/java/com/example/androiduidesign/SplashScreenActivity.kt
@@ -1,32 +1,36 @@
 package com.example.androiduidesign
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
-import com.example.androiduidesign.authencation.SignInActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androiduidesign.onboardingscreen.OnBoardingActivity
 import com.example.androiduidesign.utils.TWOTHOUSAND
 
+
 class SplashScreenActivity : AppCompatActivity() {
     lateinit var handler : Handler
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        handler  = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            startActivity(Intent(this,OnBoardingActivity::class.java))
-            finish()
-        }, TWOTHOUSAND.toLong())
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        initialSetup()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         handler.removeCallbacksAndMessages(null)
+    }
+
+    private fun initialSetup() {
+        supportActionBar?.hide()
+        handler  = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            finish()
+        }, TWOTHOUSAND.toLong())
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
