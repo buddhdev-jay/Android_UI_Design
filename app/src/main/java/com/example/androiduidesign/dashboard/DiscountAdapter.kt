6dabcba1477abc081f5996468011a4c395041a3e
buddhdev-jay <jay.b@simformsolutions.com>
2022-05-16 16:31:47 +0530

    Home Screen 70% Completed

---
+++ app/src/main/java/com/example/androiduidesign/dashboard/DiscountAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.androiduidesign.dashboard
+
+class DiscountAdapter {
+}
---
+++ app/src/main/java/com/example/androiduidesign/dashboard/DiscountItemModel.kt
@@ -0,0 +1,4 @@
+package com.example.androiduidesign.dashboard
+
+class DiscountItemModel(val image: Int) {
+}
